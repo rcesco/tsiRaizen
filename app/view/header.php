90f16b5e5d65e572ed7984fe907f21d2f164618d
Marcelo Borges <marcelo.ro.borges@gmail.com>
2023-06-27 19:53:53 -0300

    cadastros marcelo

--- app/view/header.php
+++ app/view/header.php
@@ -162,31 +162,18 @@
           <li class="nav-item">
             <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button"
                aria-expanded="false" aria-controls="sidebarDashboards">
-              <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dashboards</span>
+              <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Cadastros</span>
             </a>
             <div class="collapse menu-dropdown" id="sidebarDashboards">
               <ul class="nav nav-sm flex-column">
                 <li class="nav-item">
-                  <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics"> Analytics </a>
+                  <a href="/user" class="nav-link" data-key="t-analytics"> Usuário </a>
                 </li>
                 <li class="nav-item">
-                  <a href="dashboard-crm.html" class="nav-link" data-key="t-crm"> CRM </a>
+                  <a href="/profile" class="nav-link" data-key="t-crm"> Perfil </a>
                 </li>
                 <li class="nav-item">
-                  <a href="index.html" class="nav-link" data-key="t-ecommerce"> Ecommerce </a>
-                </li>
-                <li class="nav-item">
-                  <a href="dashboard-crypto.html" class="nav-link" data-key="t-crypto"> Crypto </a>
-                </li>
-                <li class="nav-item">
-                  <a href="dashboard-projects.html" class="nav-link" data-key="t-projects"> Projects </a>
-                </li>
-                <li class="nav-item">
-                  <a href="dashboard-nft.html" class="nav-link" data-key="t-nft"> NFT</a>
-                </li>
-                <li class="nav-item">
-                  <a href="dashboard-job.html" class="nav-link"><span data-key="t-job">Job</span> <span
-                      class="badge badge-pill bg-success" data-key="t-new">New</span></a>
+                  <a href="/transporter" class="nav-link" data-key="t-ecommerce"> Transportadora </a>
                 </li>
               </ul>
             </div>
